34938fa7657c0275a0b83979b486fde3a748603f
lawrence <lawrence@10.242.1.41>
2019-09-05 12:27:09 -0400

    ready

---
+++ HelloThereClass1/src/TenLines.java
@@ -0,0 +1,23 @@
+
+public class TenLines {
+
+	public static void main(String[] args) {
+		
+
+		// Input the message that will display to the user.
+		System.out.println("Class Schedule");
+		System.out.println("Mondays and Wednesdays:");
+		System.out.println("SCY 391 Computer Networks Wired and Wireless - 12:40p.m. until 1:55p.m.");
+		System.out.println("SCS 270 Computer Forensics Desktop and Mobile - 2:15p.m. until 3:30p.m.");
+		System.out.println("Tuesdays and Thursdays:");
+		System.out.println("SCS 132 Prog 1: Intro to Development - 11:10a.m. until 12:25p.m.");
+		System.out.println("SBU 100 Principles of Accounting I - 12:45p.m. until 2:00p.m.");
+		System.out.println("Update:");
+		System.out.println("Mondays and Wednesdays classes will be in room 106 of Lynch Hall");
+		System.out.println("Tuesdays and Thursdays classes will be in room 405 of the Admin building");
+		
+		
+		
+	}
+
+}
---
+++ HelloThereClass1/src/classpractice.java
@@ -0,0 +1,15 @@
+
+public class classpractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Project Name: Hello There
+		//Name: Lawrence Isles
+		
+		System.out.println("Hello There!");
+		
+
+	}
+
+}
