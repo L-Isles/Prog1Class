e637c1e0b8e4a03d60da769a0b1418a1d9d49079
lawrence <lawrence@lawrences-air>
2019-10-08 19:05:49 -0400

    done

---
+++ HelloThereClass1/src/Account.java
@@ -0,0 +1,22 @@
+// Lawrence Isles
+// Topic 3
+
+// Import Required Code (Import code for keyboard)
+import java.util.Scanner;
+
+public class Account {
+public static void main(String args[]) {
+	
+}
+
+		
+		// Start keyboard scanner
+		Scanner keyboard = new Scanner(System.in);
+		
+	System.out.println("Welcome to your Bank Account");
+	System.out.println("Please make a selection from the following choices:");
+	System.out.println("1. Deposit");
+	System.out.println("2. Withdraw");
+	
+	
+}
---
+++ HelloThereClass1/src/IslesProgram1.java
@@ -0,0 +1,20 @@
+// Program 1 Creating variables
+public class IslesProgram1 {
+
+	public static void main(String[] args) {
+		// Declare variables
+		int age = 37;
+		byte favoriteNumber = 64;
+		String name = "Lawrence";
+		// Print the variables
+		System.out.println("My name is " + name);
+		System.out.println("I am " + age + " years old");
+		System.out.print("My favorite number is " + favoriteNumber);
+		
+		
+		
+		
+		
+	}
+
+}
